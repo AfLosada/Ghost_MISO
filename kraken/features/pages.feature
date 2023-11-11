8284e60e57369b9e928ba18db61113078a9f93a9
Andrés Felipe Losada luna <nedrocoli@gmail.com>
2023-11-11 11:54:44 -0500

    almos finished page kraken features

--- kraken/features/pages.feature
+++ kraken/features/pages.feature
@@ -1,6 +1,61 @@
-Feature: Pages
+Feature: Pagina
 
 @user1 @web
 Scenario: Crear una página
   Given I navigate to page "http://localhost:2368/ghost/"
-  When I have correctly signed in
+  When I log in
+  And I wait for 2 seconds
+  When I click on pages
+  When I create a new page
+  Then I put on the title "Asereje" 
+  Then I create a markdown card and fill it with "asdfg"
+  Then I return to pages
+  Then I send a signal to user 2 containing "crear pagina complete"
+
+
+@user2 @web
+Scenario: Editar una página
+  Given I navigate to page "http://localhost:2368/ghost/#/pages"
+  When I log in
+  And I wait for a signal containing "crear pagina complete" for 15 seconds
+  When I click on edit for the first page
+  Then I edit the markdown of the page by putting the "Asereje Edit"
+  Then I return to pages
+  Then I send a signal to user 3 containing "editar pagina complete"
+
+@user3 @web
+Scenario: Publicar una página 
+  Given I navigate to page "http://localhost:2368/ghost/#/pages"
+  When I log in
+  And I wait for a signal containing "editar pagina complete" for 30 seconds
+  When I click on edit for the first page
+  Then I publish the page
+  Then I return to editor
+  Then I return to pages
+  Then I filter by published pages
+  Then I send a signal to user 4 containg "publish page complete"
+
+@user4 @web
+Scenario: Programar una página para publicacion
+  Given I navigate to page "http://localhost:2368/ghost/#/pages"
+  When I log in
+  And I wait for a signal containing "publish page complete" for 45 seconds
+  When I click on edit for the first page
+  Then I schedule a page to publish
+  Then I return to editor
+  Then I return to pages
+  Then I filter by scheduled pages
+  Then I send a signal to user 5 containg "schedule page complete"
+
+
+@user5 @web
+Scenario: Dejar Programar una página que fue programada para publicarse
+  Given I navigate to page "http://localhost:2368/ghost/#/pages"
+  When I log in
+  And I wait for a signal containing "schedule page complete" for 45 seconds
+  When I click on edit for the first page
+  Then I schedule a page to publish
+  Then I return to editor
+  Then I return to pages
+  Then I filter by scheduled pages
+  Then I send a signal to user 5 containg "unschedule page complete"
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,23 +1,95 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
-When('I have correctly signed in', async () => {
-  if (this.driver.getCurrentUrl() === "http://localhost:2368/ghost/#/setup") {
-    await singup()
-    return
-  }
-  await login()
+// PAGES SECTION
+
+When('I sign in', async function () {
+  await singup(this.driver)
 })
 
-const singup = async () => {
-  await this.driver.$('#blog-title').setValue("Title")
-  await this.driver.$('#name').setValue("Grupo 5")
-  await this.driver.$('#email').setValue("nedrocoli@gmail.com")
-  await this.driver.$('#password').setValue("12345678910")
-  await this.driver.$('#ember4').click()
+When('I log in', async function () {
+  const singupAvailble = !(await(this.driver.$('#setup')).error)
+  if (singupAvailble){
+    await singup(this.driver)
+    return
+  }
+  await login(this.driver)
+})
+
+When('I click on pages', async function () {
+  await this.driver.$('a[href="#/pages/"]').click()
+})
+
+When('I create a new page', async function () {
+  await this.driver.$('a[href="#/editor/page/"]').click()
+})
+
+Then('I put on the title {string}', async function (title){
+  await this.driver.$('section > div.gh-koenig-editor.relative.z-0 > div.gh-koenig-editor-pane.flex.flex-column.mih-100 > div.gh-editor-title-container.page-improvements').setValue(title)
+})
+
+When('I create a markdown card and fill it with {string}', async function (content){
+  await this.driver.$('section > div.gh-koenig-editor.relative.z-0 > div.gh-koenig-editor-pane.flex.flex-column.mih-100 > div:nth-child(3) > div > div > div:nth-child(1)').click()
+  await this.driver.$('section > div.gh-koenig-editor.relative.z-0 > div.gh-koenig-editor-pane.flex.flex-column.mih-100 > div:nth-child(3) > div > div > div.absolute.z-50 > div > button').click()
+  await this.driver.$('section > div.gh-koenig-editor.relative.z-0 > div.gh-koenig-editor-pane.flex.flex-column.mih-100 > div:nth-child(3) > div > div > div.absolute.z-50 > div > ul > li:nth-child(1) > ul > li:nth-child(2)').click()
+  await this.driver.$('section > div.gh-koenig-editor.relative.z-0 > div.gh-koenig-editor-pane.flex.flex-column.mih-100 > div:nth-child(3) > div > div > div:nth-child(1) > div > div > div > div > div > div.CodeMirror.cm-s-paper.CodeMirror-wrap > div.CodeMirror-scroll > div').setValue(content)
+})
+
+Then('I return to pages', async function() {
+  await this.driver.$('a[href="#/pages/"]').click()
+})
+
+Then('I return to editor', async function() {
+  await this.driver.$('div > div > header > button').click()
+})
+
+When('I click on edit for the first page', async function() {
+  await this.driver.$('body > div.gh-app > div > main > section > section > div > div:nth-child(1) > li > a').click()
+})
+
+Then('I edit the markdown of the page by putting the {string}', async function(content) {
+  const markdown = this.driver.$('section > div.gh-koenig-editor.relative.z-0 > div.gh-koenig-editor-pane.flex.flex-column.mih-100 > div:nth-child(3) > div > div > div:nth-child(1) > div > div')
+  await markdown.click()
+  const editorButton = this.driver.$('section > div.gh-koenig-editor.relative.z-0 > div.gh-koenig-editor-pane.flex.flex-column.mih-100 > div:nth-child(3) > div > div > div:nth-child(1) > div > div > div > div > ul > li:nth-child(1)')
+  await editorButton.click()
+  await this.driver.$('section > div > div > div:nth-child(3) > div > div > div:nth-child(1) > div > div > div > div > div > div.CodeMirror.cm-s-paper.CodeMirror-wrap > div.CodeMirror-scroll > div').setValue(content)
+})
+
+Then('I publish the page', async function () {
+  await this.driver.$('section > header > section > button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger').click()
+  await this.driver.$('div > div > div > div.gh-publish-cta > button').click()
+  await this.driver.$('div > div > div.gh-publish-cta > button').click()
+})
+
+Then('I schedule a page to publish', async function () {
+  await this.driver.$('section > header > section > button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger').click()
+  await this.driver.$('div > div > div > div.gh-publish-settings > div.gh-publish-setting.last > button').click()
+  await this.driver.$('div > div > fieldset > div > div:nth-child(2)').click()
+  await this.driver.$('div > div > div > div.gh-publish-cta > button').click()
+  await this.driver.$('div > div > div.gh-publish-cta > button').click()
+})
+
+Then('I filter by published pages', async function() {
+  await this.driver.$('body > div.gh-app > div > main > section > div > header > section > div > div.gh-contentfilter-menu.gh-contentfilter-type').click()
+  await this.driver.$('body > div > div > ul > :nth-child(3)').click()
+})
+
+Then('I filter by scheduled pages', async function() {
+  await this.driver.$('body > div.gh-app > div > main > section > div > header > section > div > div.gh-contentfilter-menu.gh-contentfilter-type').click()
+  await this.driver.$('body > div > div > ul > :nth-child(4)').click()
+})
+
+// LOGIN UTILITIES
+
+const singup = async function (driver) {
+  await driver.$('#blog-title').setValue("Title")
+  await driver.$('#name').setValue("Grupo 5")
+  await driver.$('#email').setValue("nedrocoli@gmail.com")
+  await driver.$('#password').setValue("12345678910")
+  await driver.$('button:contains("Create account & start publishing →)"').click()
 }
 
-const login = async () => {
-  await this.driver.$('#identification').setValue("nedrocoli@gmail.com")
-  await this.driver.$('#password').setValue("12345678910")
-  await this.driver.$('#ember5').click()
+const login = async function (driver) {
+  await driver.$('#identification').setValue("nedrocoli@gmail.com")
+  await driver.$('#password').setValue("12345678910")
+  await driver.$('button.login').click()
 }
