a6707e94e8b9ef7cbe15095498a73145f0fe4280
Jader Pelaez <j.pelaezg@uniandes.edu.co>
2023-11-18 23:58:22 -0500

    cypress crear tag funcional

---
+++ pruebas-e2e/cypress/cypress/e2e/CrearTag.cy.js
@@ -0,0 +1,19 @@
+describe('Crear tag', () => {
+    it('Se crea un tag desde el inicio de sesión hasta la finalización de la creación', () => {
+      cy.login('nedrocoli@gmail.com','12345678910')
+      cy.get('a[href="#/tags/"]').click()
+      cy.wait(2000)
+      cy.get('a[href="#/tags/new/"]:first').click()
+      cy.wait(2000)
+      cy.get('#tag-name').type('AAAAAAAAAA Tag prueba')
+      cy.wait(2000)
+      cy.get('#tag-description').type('Se crea un tag de prueba para validar la funcionalidad')
+      cy.wait(2000)
+      cy.get('#ember57 > span').click()
+      //cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click()
+      cy.wait(2000)
+      cy.get('li:has(a[href="#/tags/"])').click()
+      cy.wait(2000)
+      cy.get("h3.gh-tag-list-name").contains("AAAAAAAAAA Tag prueba").should('exist');
+    })
+  })
