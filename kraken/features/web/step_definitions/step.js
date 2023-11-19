05a0f241a0e9aa48294330310df9b6cf99b825ba
Eduardo Benito <e.benito@uniandes.edu.co>
2023-11-19 15:02:08 -0500

    Ajuste pruebas tags Kraken

--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -173,6 +173,19 @@ const login = async function (driver) {
 
 // TAG SECTION
 
+When('I sign in', async function () {
+  await singup(this.driver)
+})
+
+When('I log in', async function () {
+  const singupAvailble = !(await(this.driver.$('#setup')).error)
+  if (singupAvailble){
+    await singup(this.driver)
+    return
+  }
+  await login(this.driver)
+})
+
 When('I click on tags', async function () {
   await this.driver.$('a[href="#/tags/"]').click()
 })
@@ -180,6 +193,59 @@ When('I click on tags', async function () {
 When('I create a new tag', async function () {
   await this.driver.$('a[href="#/tags/new/"]').click()
 })
+
+Then('I put on the name {string}', async function (name){
+  await this.driver.$('div > div.gh-tag-settings-multiprop > div.form-group.mr2.flex-auto > input#tag-name.gh-input').setValue(name)
+})
+Then('I put on the color {string}', async function (color){
+  await this.driver.$('div > div.gh-tag-settings-multiprop > div.form-group.gh-tag-settings-colorcontainer > div.input-color > input.gh-input').setValue(color)
+})
+Then('I click on save', async function(){
+  await this.driver.$('form.mb15 > div.gh-canvas-header > header.gh-canvas-header-content > section.view-actions > button#ember743.gh-btn-primary.gh-btn-icon.ember-view').click()
+})
+Then('I return to tags', async function() {
+  await this.driver.$('a[href="#/tags/"]').click()
+})
+
+When('I create a new internal tag', async function () {
+  await this.driver.$('a[href="#/tags/new/"]').click()
+})
+
+Then('I put on the name {string}', async function (Internalname){
+  await this.driver.$('div > div.gh-tag-settings-multiprop > div.form-group.mr2.flex-auto > input#tag-name.gh-input').setValue(Internalname)
+})
+Then('I put on the color {string}', async function (Internalcolor){
+  await this.driver.$('div > div.gh-tag-settings-multiprop > div.form-group.gh-tag-settings-colorcontainer > div.input-color > input.gh-input').setValue(Internalcolor)
+})
+Then('I click on save', async function(){
+  await this.driver.$('form.mb15 > div.gh-canvas-header > header.gh-canvas-header-content > section.view-actions > button#ember743.gh-btn-primary.gh-btn-icon.ember-view').click()
+})
+Then('I return to tags', async function() {
+  await this.driver.$('a[href="#/tags/"]').click()
+})
+When('I edit a tag', async function(){
+  await this.driver.$('section.view-container.content-list > ol.tags-list.gh-list').first().click()
+})
+Then('I delete a tag',async function(){
+  await this.driver.$('section.gh-canvas > div > button.gh-btn.gh-btn-red.gh-btn-icon').click()
+  await this.driver.$('div#ember1351.epm-modal.fullscreen-modal-action.fullscreen-modal-wide > div.modal-content > div.modal-footer > button#ember1352.gh-btn.gh-btn-red.gh-btn-icon.ember-view').click()
+})
+When('I edit a tag', async function(){
+  await this.driver.$('section.view-container.content-list > ol.tags-list.gh-list').first().click()
+})
+Then('I change the name', async function(name){
+  await this.driver.$('div > div.gh-tag-settings-multiprop > div.form-group.mr2.flex-auto > input#tag-name.gh-input').setValue(name)
+})
+Then('I change the color {string}', async function (color){
+  await this.driver.$('div > div.gh-tag-settings-multiprop > div.form-group.gh-tag-settings-colorcontainer > div.input-color > input.gh-input').setValue(color)
+})
+Then('I click on save', async function(){
+  await this.driver.$('form.mb15 > div.gh-canvas-header > header.gh-canvas-header-content > section.view-actions > button#ember743.gh-btn-primary.gh-btn-icon.ember-view').click()
+})
+Then('I return to tags', async function() {
+  await this.driver.$('a[href="#/tags/"]').click()
+})
+
 
 //Funcionalidad editar perfil 
 
