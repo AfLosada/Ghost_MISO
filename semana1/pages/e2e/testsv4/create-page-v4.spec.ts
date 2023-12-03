37a63a842fb5287d9e184bc4673012261c7f8ef1
Andrés Felipe Losada luna <nedrocoli@gmail.com>
2023-12-02 19:03:10 -0500

    finishing page tests for v4

--- semana1/pages/e2e/testsv4/create-page-v4.spec.ts
+++ semana1/pages/e2e/testsv4/create-page-v4.spec.ts
@@ -11,17 +11,17 @@ test.describe('Funcionalidades sobre página: Create pages', () => {
     await page.getByRole('link', { name: 'New page' }).click()
     await page.screenshot({ path: 'testsv4/images-results/create-page/markdown-image/new-page.png', fullPage: true })
     await page.getByPlaceholder('Page title').click()
-    await page.getByPlaceholder('Page title').fill('asereje')
+    await page.getByPlaceholder('Page title').fill('Markdown')
     await page.locator('.koenig-editor__editor').click()
     await page.getByLabel('Add a card').click()
     await page.waitForTimeout(100)
-    await page.getByRole('menuitem', { name: 'Markdown' }).waitFor();
-    await page.getByRole('menuitem', { name: 'Markdown' }).click();
+    await page.getByRole('menuitem', { name: 'Markdown' }).waitFor()
+    await page.getByRole('menuitem', { name: 'Markdown' }).click()
     await page.getByLabel('Add a card').click()
     await page.screenshot({ path: 'testsv4/images-results/create-page/markdown-image/card-menu.png', fullPage: true })
     await page.waitForTimeout(100)
-    await page.getByRole('menuitem', { name: 'Markdown' }).waitFor();
-    await page.getByRole('menuitem', { name: 'Markdown' }).click();
+    await page.getByRole('menuitem', { name: 'Markdown' }).waitFor()
+    await page.getByRole('menuitem', { name: 'Markdown' }).click()
     await page.waitForTimeout(10000)
     await page.locator('.CodeMirror-scroll').click()
     await page.getByTitle('Heading (Ctrl-H)').click()
@@ -33,8 +33,45 @@ test.describe('Funcionalidades sobre página: Create pages', () => {
     })
     await page.screenshot({ path: 'testvs4/images/edit-page.png', fullPage: true })
     await page.getByRole('link', { name: 'Pages' }).click()
-    await page.getByRole('link', { name: 'asereje By Grupo 5 ' }).nth(0).click()
+    await page.getByRole('link', { name: 'Markdown' }).nth(0).click()
     expect(page.locator('pre').filter({ hasText: 'headeroooo' })).toBeTruthy()
+  })
+
+  test('GIVEN I can login, WHEN I create a page and add a spotify embed', async ({ page }) => {
+    await page.goto('http://localhost:2368/ghost')
+    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/main-page.png', fullPage: true })
+    await page.getByRole('link', { name: 'Pages', exact: true }).click()
+    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/pages.png', fullPage: true })
+    await page.getByRole('link', { name: 'New page' }).click()
+    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/new-page.png', fullPage: true })
+    await page.getByPlaceholder('Page title').click()
+    await page.getByPlaceholder('Page title').fill('Spotify Embed')
+    await page.locator('.koenig-editor__editor').click()
+    await page.getByLabel('Add a card').click()
+    await page.waitForTimeout(100)
+    await page.getByRole('menuitem', { name: 'Spotify' }).waitFor()
+    await page.getByRole('menuitem', { name: 'Spotify' }).click()
+    await page.waitForTimeout(100)
+    await page.getByLabel('Add a card').click()
+    await page.getByRole('menuitem', { name: 'Spotify' }).waitFor()
+    await page.getByRole('menuitem', { name: 'Spotify' }).click()
+    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/card-menu.png', fullPage: true })
+    await page
+      .getByPlaceholder('Paste URL to add embedded content...')
+      .fill('https://open.spotify.com/intl-es/track/7EKysfHBIwPQ3RvhD9dH9D?si=3b1d1455e2ae4413')
+    await page.screenshot({
+      path: 'testsv4/images-results/create-page/spotify-embed/fill-spotify-menu.png',
+      fullPage: true,
+    })
+    await page.keyboard.down('Enter')
+    await page.waitForSelector('iframe')
+    await page.screenshot({
+      path: 'testsv4/images-results/create-page/spotify-embed/create-page-spotify-menu.png',
+      fullPage: true,
+    })
+    await page.getByRole('link', { name: 'Pages' }).click()
+    await page.getByRole('link', { name: 'Spotify Embed' }).first().click()
+    expect(page.locator('iframe')).toBeVisible()
   })
 
   test('GIVEN I can log in the page, WHEN I create a page and add a feature image THEN the page has a feature image', async ({
@@ -57,45 +94,11 @@ test.describe('Funcionalidades sobre página: Create pages', () => {
       fullPage: true,
     })
     await page.getByRole('link', { name: 'Pages' }).click()
-    await page
-      .locator('body > div.gh-app > div > main > section > section > div > div:nth-child(1) > li:first-child > a', {
-        hasText: 'Feature Image',
-      })
-      .click()
+    await page.getByRole('link', { name: 'Feature Image' }).first().click()
     await page.screenshot({
       path: 'testsv4/images-results/create-page/create-feature-image-page-result.png',
       fullPage: true,
     })
-    expect(page.getByRole('link', { name: 'Feature Image' })).toBeTruthy()
-  })
-
-  test('GIVEN I can login, WHEN I create a page and add a spotify embed', async ({ page }) => {
-    await page.goto('http://localhost:2368/ghost')
-    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/main-page.png', fullPage: true })
-    await page.getByRole('link', { name: 'Pages', exact: true }).click()
-    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/pages.png', fullPage: true })
-    await page.getByRole('link', { name: 'New page' }).click()
-    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/new-page.png', fullPage: true })
-    await page.getByPlaceholder('Page title').click()
-    await page.getByPlaceholder('Page title').fill('Spotify Embed')
-    await page.getByRole('paragraph').click()
-    await page.getByLabel('Add a card').click()
-    await page.screenshot({ path: 'testsv4/images-results/create-page/spotify-embed/card-menu.png', fullPage: true })
-    await page.getByRole('menuitem', { name: 'Spotify' }).click()
-    await page
-      .getByTestId('embed-url')
-      .fill('https://open.spotify.com/intl-es/track/7EKysfHBIwPQ3RvhD9dH9D?si=3b1d1455e2ae4413')
-    await page.screenshot({
-      path: 'testsv4/images-results/create-page/spotify-embed/fill-spotify-menu.png',
-      fullPage: true,
-    })
-    await page.keyboard.down('Enter')
-    await page.screenshot({
-      path: 'testsv4/images-results/create-page/spotify-embed/create-page-spotify-menu.png',
-      fullPage: true,
-    })
-    await page.getByRole('link', { name: 'Pages' }).click()
-    await page.getByRole('link', { name: 'Spotify Embed' }).first().click()
-    expect(page.getByTestId('embed-iframe')).toBeVisible()
+    expect(page.locator('img')).toBeInViewport()
   })
 })
